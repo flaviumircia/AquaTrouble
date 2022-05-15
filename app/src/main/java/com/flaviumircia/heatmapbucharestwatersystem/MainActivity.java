7ddb8b1a3a2d98239982902382041b528c2a11a2
flaviumircia <flaviumircia15@gmail.com>
2022-05-15 12:51:32 +0300

    Start building the app

---
+++ app/src/main/java/com/flaviumircia/heatmapbucharestwatersystem/MainActivity.java
@@ -0,0 +1,14 @@
+package com.flaviumircia.heatmapbucharestwatersystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//building stage
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
